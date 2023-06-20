6e1010daea595ddd36ef9b275305f9c34a373c62
cockrll <cockerell.korkora@gmail.com>
2023-06-20 14:22:08 +0300

    messages are added to a list

--- src/main/java/com/micro/microsender/controller/SenderController.java
+++ src/main/java/com/micro/microsender/controller/SenderController.java
@@ -12,16 +12,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.text.Normalizer;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 
 @Controller
 public class SenderController {
 
-//    @Autowired
-//    private SenderService senderService;
-
     @Autowired
     private RabbitTemplate rabbitTemplate;
+
+    Map<User, List<String>> userMessages = new ConcurrentHashMap<>();
 
     private static final Logger logger = LogManager.getLogger(MicroSenderApplication.class);
 
@@ -33,13 +34,11 @@ public class SenderController {
     }
 
     @RequestMapping(value="/user", headers = "Accept=application/json", method= RequestMethod.POST)
-    public String send(@ModelAttribute("user") User user, Model model){
+    public String send(@ModelAttribute("user") User user, Model model) {
         logger.info(user);
         model.addAttribute("user", user);
-//        senderService.sendMessage(user);
-        rabbitTemplate.convertAndSend(Constants.EXCHANGE,Constants.ROUTING_KEY, user.getMessage());
-        logger.info(user.getMessage(), " is sent" );
+        user.addMessagesToMap();
+        rabbitTemplate.convertAndSend(Constants.EXCHANGE, Constants.ROUTING_KEY, user.getMessage());
         return "user-page";
     }
-
 }
--- src/main/java/com/micro/microsender/model/User.java
+++ src/main/java/com/micro/microsender/model/User.java
@@ -1,19 +1,67 @@
 package com.micro.microsender.model;
 
-import org.springframework.stereotype.Component;
+import com.micro.microsender.MicroSenderApplication;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-public class User {
+import java.util.*;
+
+public class User implements Comparable<User> {
     private String name;
     private String surname;
     private String message;
+    private static Map<User, List<String>> userMessages = new HashMap<User,List<String>>();
+    private static final Logger logger = LogManager.getLogger(MicroSenderApplication.class);
 
     public User() {
+    }
+
+    public User(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
     }
 
     public User(String name, String surname, String message) {
         this.name = name;
         this.surname = surname;
         this.message = message;
+        addMessagesToMap();
+    }
+
+    public void addMessagesToMap(){
+        if (userMessages.isEmpty()) {
+            userMessages.put(new User(this.name, this.surname), Arrays.asList(this.message));
+            logger.info("user is added into the empty list: ", this);
+        }
+        else {
+            for(Iterator<Map.Entry<User,List<String>>> iterator = userMessages.entrySet().iterator(); iterator.hasNext();){
+                Map.Entry<User,List<String>> entry = iterator.next();
+                User key = entry.getKey();
+                if((key.getSurname().equals(this.surname) &&
+                        key.getName().equals(this.name))){
+
+                    logger.info("existing user: ", this);
+                    List<String> messagesList = entry.getValue();
+
+                    // -------------------------------- //
+
+                    List<String> merged = new ArrayList<>();
+                    merged.addAll(messagesList);
+
+                    // --------------------------------- //
+
+                    System.out.println("info: " + this + " " + merged.size());
+                    entry.setValue(merged);
+                    for(String m: merged){
+                        System.out.println("message: " + m);
+                    }
+                    userMessages.put(key,merged);
+                } else {
+                    logger.info("not existing user: ", this);
+                    userMessages.put(new User(this.name, this.surname), Arrays.asList(this.message));
+                }
+            }
+        }
     }
 
     public String getName() {
@@ -40,6 +88,10 @@ public class User {
         this.message = message;
     }
 
+    public static Map<User, List<String>> getUserMessages() {
+        return userMessages;
+    }
+
     @Override
     public String toString() {
         return "User{" +
@@ -47,5 +99,17 @@ public class User {
                 ", surname='" + surname + '\'' +
                 ", message='" + message + '\'' +
                 '}';
+    }
+
+    @Override
+    public int compareTo(User user) {
+        // if the string are not equal
+        if (this.name.compareTo(user.name) != 0) {
+            return this.name.compareTo(user.name);
+        } else
+        if(this.surname.compareTo(user.surname) != 0){
+            return this.surname.compareTo(user.surname);
+        }
+        return -1;
     }
 }
